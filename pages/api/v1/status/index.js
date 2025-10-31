6a00b4150a174239d3d87730c61b3c80be8f9f72
José Guilherme <jose.guilherme96@outlook.com.br>
2025-10-31 01:30:22 +0000

    finish `/api/v1/status` endpoint

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,9 +1,35 @@
 import database from 'infra/database.js'
+import totalCount from 'infra/database.js'
+
 
 async function status(request, response) {
 
-  const result = await database.query("SELECT 5 + 1");
-  console.log(result.rows);
-  response.status(200).json({ message: "Status API" });
+  const updatedAt = new Date().toISOString()
+
+  const databaseVersionResult = await database.query("SHOW server_version;");
+  const databaseVersionValue = databaseVersionResult.rows[0].server_version;
+
+  const databaseMaxConnectionsResult = await database.query("show max_connections;")
+  const databaseMaxConnectionsValue = databaseMaxConnectionsResult.rows[0].max_connections;
+  
+  const databaseName = process.env.POSTGRES_DB
+  const databaseOpenedConnectionsResult = await database.query({
+    text:"SELECT COUNT(*)::int FROM pg_stat_activity WHERE datname = $1;",
+    values:[databaseName]
+  })
+  const databaseOpenedConnectionValue = databaseOpenedConnectionsResult.rows[0].count
+  
+  response.status(200).json({ 
+    "updated_at": updatedAt , 
+    "dependencies":{
+      "database":{
+        "version_database":databaseVersionValue,
+        "opened_connections":1,
+        "max_connections": parseInt(databaseMaxConnectionsValue),
+        "opened_connetions": databaseOpenedConnectionValue
+      }
+    } 
+  });
+  
 }
 export default status;
