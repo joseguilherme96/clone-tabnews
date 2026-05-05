5ae18bfa5f22ffc4907dff85e803b203d5a93ac2
José Guilherme <jose.guilherme96@outlook.com.br>
2026-05-05 11:58:08 -0300

    refactor: remove properties from unused tags (#62)
    * feat: adds page `status`
    
    * fix: centralizes requests
    
    * fix: remove the `components` from inside the `pages` folder
    
    * fix: remove object with keys `{isLoading, data}` within React
    
    * refactor: call the `useSWRFecthAPIStatus` function directly in the component
    
    * refactor: remove properties from unused tags

--- components/Status/DataBase/DataBase.jsx
+++ components/Status/DataBase/DataBase.jsx
@@ -3,13 +3,13 @@ import MaxConnections from "./MaxConnections";
 import OpenedConnetions from "./OpenedConnetions";
 import ColorTextDefault from "../../Color/ColorTextDefault";
 
-function DataBase(api_status) {
+function DataBase() {
   return (
     <>
       <ColorTextDefault descricao="Data Base" />
-      <Version response={api_status.response} />
-      <MaxConnections response={api_status.response} />
-      <OpenedConnetions response={api_status.response} />
+      <Version />
+      <MaxConnections />
+      <OpenedConnetions />
     </>
   );
 }
