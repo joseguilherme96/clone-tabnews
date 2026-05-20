80d8f24427f552950762b95266a0b43b4cb5b536
José Guilherme <jose.guilherme96@outlook.com.br>
2026-05-20 17:45:42 +0000

    fix: create and use `InternalServerError` in `/api/v1/status`

---
+++ infra/errors.js
@@ -0,0 +1,23 @@
+export class InternalServerError extends Error {
+  constructor({ cause }) {
+    super("Um erro interno não esperado aconteceu.", { cause });
+
+    this.name = "InternalServerError";
+    this.action = "Entre em contato com o suporte.";
+    this.statusCode = 500;
+
+    console.info(
+      "Dentro do constructor da classe `InternalServerError` do arquivo infra/errors.js",
+    );
+    console.error(cause);
+  }
+
+  toJSON() {
+    return {
+      name: this.name,
+      message: this.message,
+      action: this.action,
+      status_code: this.statusCode,
+    };
+  }
+}
