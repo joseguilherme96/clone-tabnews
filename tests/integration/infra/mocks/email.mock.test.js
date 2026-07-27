0bde23d19d85c8395cb2cc04fb0495094d122d63
José GUilherme <jose.guilherme96@outlook.com.br>
2026-07-27 15:31:07 -0300

    test: adds expect mock

--- tests/integration/infra/mocks/email.mock.test.js
+++ tests/integration/infra/mocks/email.mock.test.js
@@ -1,6 +1,10 @@
 import { send } from "infra/email.js";
 import orchestrator, { waitForAllServices } from "tests/orchestrator.js";
-import { deleteAllEmails } from "tests/orchestrator.email.js";
+import {
+  deleteAllEmails,
+  getEmails,
+  getEmailById,
+} from "tests/orchestrator.email.js";
 
 jest.mock("infra/email.js", () => {
   const originalModule = jest.requireActual("infra/email.js");
@@ -78,7 +82,41 @@ describe("infra/email.test.js", () => {
       text: "Corpo do ultimo email enviado.",
     });
 
+    expect(send.mock.calls[0][0]).toEqual({
+      from: '"Example Team" <team@example.com>',
+      to: "alice@example.com, bob@example.com",
+      subject: "Hello",
+      text: "Hello world?",
+    });
+    expect(send.mock.calls[1][0]).toEqual({
+      from: '"Example Team" <team@example.com>',
+      to: "alice@example.com, bob@example.com",
+      subject: "Último email enviado",
+      text: "Corpo do ultimo email enviado.",
+    });
+    expect(send.mock.results[0].value).toBe(true);
+
     const lastEmail = await orchestrator.getLastEmail();
+
+    expect(getEmails.mock.results[0].value).toEqual([
+      {
+        id: 1,
+        sender: "<team@example.com>",
+        recipients: ["<alice@example.com>", "<bob@example.com>"],
+        subject: "Último email enviado",
+        size: "357",
+        created_at: "2025-08-30T19:28:58+00:00",
+        text: "Corpo do ultimo email enviado.\r\n",
+      },
+    ]);
+    expect(getEmails.mock.calls[0].length).toBe(0);
+    expect(getEmails.mock.calls[0][0]).toBe(undefined);
+
+    expect(getEmailById.mock.results[0].value).toBe(
+      "Corpo do ultimo email enviado.\r\n",
+    );
+    expect(getEmailById.mock.calls[0].length).toBe(1);
+    expect(getEmailById.mock.calls[0][0]).toBe(2);
 
     expect(lastEmail.id).toBe(2);
     expect(lastEmail.sender).toBe("<team@example.com>");
